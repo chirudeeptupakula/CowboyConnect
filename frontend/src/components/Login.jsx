2eb33b87a52e04e48634c02bb5037822d7f0db87
yeshwanthkj <abinavayeshwanthkj@gmail.com>
2025-04-18 01:48:05 -0500

    Updated front end login and register pages

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -33,12 +33,7 @@ function Login() {
   return (
     <>
       <Header />
-      <div
-        className="login-page"
-        style={{
-          backgroundImage: `url('/Background_login.png')`, // from public folder
-        }}
-      >
+      <div className="login-page">
         <div className="login-container">
           <input
             type="text"
